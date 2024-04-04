2685948c8173bfc6e4280babc8f33df1f15fe5e4
Alex Fourmy <alexfourmy@gmail.com>
2024-04-04 20:39:39 +0100

    tried a new logic for the all staff button

--- src/App.js
+++ src/App.js
@@ -1,83 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import NameList from './components/NameList';
-import PairingsTable from './components/PairingsTable';
-import Roulette from './components/Roulette';
-import { loadNames, saveNames } from './utils';
-import everyoneTVLogo from './img/ETV_logo.svg';
-import footerImage from './img/ETV_footer.png';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import NameList from './components/NameList'
+import Roulette from './components/Roulette'
+import { loadNames, saveNames } from './utils'
+import everyoneTVLogo from './img/ETV_logo.svg'
+import footerImage from './img/ETV_footer.png'
+import './App.css'
 
 function App() {
-  const [names, setNames] = useState([]);
-  const [pairings, setPairings] = useState([]);
+  const [names, setNames] = useState([])
+  const [pairings, setPairings] = useState([])
 
   useEffect(() => {
-    const storedNames = loadNames();
-    setNames(storedNames);
-  }, []);
+    const storedNames = loadNames()
+    setNames(storedNames)
+  }, [])
 
-  const handleAddName = (name) => {
-    setNames([...names, name]);
-    saveNames([...names, name]);
-  };
+  const handleAddName = name => {
+    setNames([...names, name])
+    saveNames([...names, name])
+  }
 
-  const handleRemoveName = (index) => {
-    const updatedNames = [...names];
-    updatedNames.splice(index, 1);
-    setNames(updatedNames);
-    saveNames(updatedNames);
-  };
+  const handleRemoveName = index => {
+    const updatedNames = [...names]
+    updatedNames.splice(index, 1)
+    setNames(updatedNames)
+    saveNames(updatedNames)
+  }
 
   const handlePairNames = () => {
-    if (names.length < 2) return;
-  
-    const available = names.filter((_, i) => !pairings.some(pair => pair.includes(i)));
-    const indices = [];
-  
+    if (names.length < 2) return
+
+    const available = names.filter(
+      (_, i) => !pairings.some(pair => pair.includes(i))
+    )
+    const indices = []
+
     while (indices.length < 2) {
-      const randomIndex = Math.floor(Math.random() * available.length);
+      const randomIndex = Math.floor(Math.random() * available.length)
       if (!indices.includes(randomIndex)) {
-        indices.push(randomIndex);
+        indices.push(randomIndex)
       }
     }
-  
-    const pair = indices.map(i => available[i]);
-    setPairings([...pairings, pair]);
-  };
 
-  // const handleResetNames = () => {
-  //   setNames([]);
-  //   setPairings([]);
-  //   saveNames([]);
-  // };
+    const pair = indices.map(i => available[i])
+    setPairings([...pairings, pair])
+  }
 
   const handleResetPairings = () => {
-    setPairings([]);
-  };
+    setPairings([])
+  }
 
   const handleRemoveAllNames = () => {
-    setNames([]);
-    saveNames([]);
-  };
+    setNames([])
+    saveNames([])
+  }
 
   return (
     <div className="App">
       <img src={everyoneTVLogo} alt="EveryoneTV Logo" className="logo" />
       <h1 className="appTitle">ETV Coffee Roulette</h1>
       <div className="appContent">
-        <NameList names={names} onAddName={handleAddName} onRemoveName={handleRemoveName} />
+        <NameList
+          names={names}
+          onAddName={handleAddName}
+          onRemoveName={handleRemoveName}
+        />
         <div className="buttons-container">
-          <button className="button-action" onClick={handleRemoveAllNames}>Remove All Names</button>
+          <button className="button-action" onClick={handleRemoveAllNames}>
+            Remove All Names
+          </button>
           <Roulette onPairNames={handlePairNames} />
           {/* <PairingsTable pairings={pairings} /> */}
-          <button className="button-action" onClick={handleResetPairings}>Reset Pairings</button>
+          <button className="button-action" onClick={handleResetPairings}>
+            Reset Pairings
+          </button>
         </div>
       </div>
-      <div className='footer-container'>
+      <div className="footer-container">
         <img src={footerImage} alt="Footer" className="footer" />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,52 +1,73 @@
-import React, { useState } from 'react';
-import userIcon from '../img/user-icon.svg';
- import userIconWhite from '../img/user-icon-white.svg';
-import { employeeList } from '../employeeList'; //
+import React, { useState } from 'react'
+import userIcon from '../img/user-icon.svg'
+import userIconWhite from '../img/user-icon-white.svg'
+import { employeeList } from '../employeeList'
 
-function NameList({ names, onAddName, onRemoveName }) {
-  const [newName, setNewName] = useState('');
+function NameList({ names, allEmployeesList, onAddName, onRemoveName }) {
+  const [newName, setNewName] = useState('')
+  const [displayEmployeeList, setDisplayEmployeeList] = useState([])
 
-  const handleInputChange = (e) => {
-    setNewName(e.target.value);
-  };
+  const handleInputChange = e => {
+    setNewName(e.target.value)
+  }
 
   const handleAddName = () => {
     if (newName.trim()) {
-      onAddName(newName.trim());
-      setNewName('');
+      onAddName(newName.trim())
+      setNewName('')
     }
-  };
+  }
 
-  const handleKeyPress = (e) => {
+  const handleKeyPress = e => {
     if (e.key === 'Enter') {
-      handleAddName();
+      handleAddName()
     }
-  };
+  }
 
-  const handleAddEmployee = () => {
-    employeeList.forEach((employee) => {
-      onAddName(employee.name);
-    });
-  };
+  const handleAddEmployees = () => {
+    setDisplayEmployeeList(employeeList.map(employee => employee.name))
+  }
+
+  // Decide which list to display based on displayEmployeeList
+  const displayList =
+    displayEmployeeList.length > 0 ? displayEmployeeList : names
 
   return (
-    <div className="name-list-container"> 
-      <div className="input-container"> 
-        <button className="button-action button-add-employee" onClick={handleAddEmployee}><img src={userIconWhite} alt='add all employees'/></button>
-        <input type="text" placeholder="Add a name" value={newName} onChange={handleInputChange} onKeyDown={handleKeyPress}/>
-        <button className="button-action" onClick={handleAddName}>Add</button>
+    <div className="name-list-container">
+      <div className="input-container">
+        <button
+          className="button-action button-add-employee"
+          onClick={handleAddEmployees}
+        >
+          <img src={userIconWhite} alt="add all employees" />
+        </button>
+        <input
+          type="text"
+          placeholder="Add a name"
+          value={newName}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyPress}
+        />
+        <button className="button-action" onClick={handleAddName}>
+          Add
+        </button>
       </div>
-      <ul className="name-list"> 
-        {names.map((name, index) => (
+      <ul className="name-list">
+        {displayList.map((name, index) => (
           <li key={index} className="name-item">
             <img src={userIcon} alt="user icon" className="user-icon" />
             {name}
-            <button className="button-remove" onClick={() => onRemoveName(index)}>X</button>
+            <button
+              className="button-remove"
+              onClick={() => onRemoveName(index)}
+            >
+              X
+            </button>
           </li>
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default NameList;
+export default NameList
