89c901dad024ba0ee53da11b251e8bd535ac9219
a4my <alexfourmy@gmail.com>
2024-06-09 10:56:27 +0100

    change: now using the same spinning loader as the roulette animation in the modal window

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const Loader = () => {
+const Roulette = () => {
   return (
-    <div className="loader-container">
-      <div className="loader">
-        <div className="outer"></div>
-        <div className="middle"></div>
-        <div className="inner"></div>
+    <div className="roulette-container">
+      <div className="roulette">
         <div className="circle"></div>
+        <div className="inner"></div>
+        <div className="middle"></div>
+        <div className="outer"></div>
+        <div className="extra-outer"></div>
       </div>
     </div>
   );
 };
 
-export default Loader;
+export default Roulette;
